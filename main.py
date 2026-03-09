2f72c2a6a1f77e7679fbf781094db03ac056becc
Filipe Cabral Coutinho <filipe_my_profile@MacBook-Pro-de-Weback.local>
2026-03-09 07:56:52 -0300

    feat: obtaining the data submitted via the form within the route

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", method=["GET", "POST"])
-def hello_world():
+@app.route("/", methods=["GET", "POST"])
+def incricao():
+    if request.method == "POST":
+        name = request.form["nomeForm"]
+        church = request.form["churchForm"]
+        celphone = request.form["celForm"]
+        email = request.form["emailForm"]
+        remedy = request.form["remedyForm"]
+        hour_remedy = request.form["hourForm"]
+
     return render_template("index.html")
 
 if __name__ == "__main__":
