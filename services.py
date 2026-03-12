c27624cde5b45339cd1657ca3a3a10a768f1446d
Filipe Cabral Coutinho <filipe_my_profile@MacBook-Pro-de-Weback.local>
2026-03-12 06:15:05 -0300

    feat: Message sending to Telegram has been implemented.

--- services.py
+++ services.py
@@ -4,19 +4,28 @@ from pathlib import Path
 import os
 import pandas as pd
 import io
+import requests
+from threading import Thread
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ROOT_PATH = Path(__file__).resolve().parent
 
 
 class Service:
+    def __init__(self):
+        self.queue_telegram = []
+
     def create_enrollment(self, new_enrollment):
         db.session.add(new_enrollment)
         db.session.commit()
 
         self.send_to_email()
-        self.send_to_telegram()
-
-        return "OK"
+        Thread(
+        target=self.send_to_telegram,
+        args=(new_enrollment, "new")
+        ).start()
 
     def get_all_enrollments(self):
         enrollments = db.session.query(Enrollment).all()
@@ -41,6 +50,11 @@ class Service:
         enrollment.payment_status = new_enrollment.payment_status
 
         db.session.commit()
+
+        Thread(
+        target=self.send_to_telegram,
+        args=(new_enrollment, "update")
+        ).start()
 
         return True
 
@@ -77,5 +91,69 @@ class Service:
     def send_to_email(self):
         pass
 
-    def send_to_telegram(self):
-        pass
+    def send_to_telegram(self, enrollment, type_msg):
+        message = f"""
+            
+            NR INSCR: {enrollment.id}
+            NOME: {enrollment.name}
+            CPF: {enrollment.cpf}
+            IGREJA: {enrollment.church}
+            CELULAR: {enrollment.celphone}
+            CTT EMERGÊNCIA: {enrollment.emergency_contact}
+            EMAIL: {enrollment.email}
+            TOMA REMÉDIO: {enrollment.remedy}
+            HORÁRIO REMÉDIO: {enrollment.hour_remedy}
+            STATUS PAGAMENTO: {enrollment.payment_status}
+            
+            """
+
+        if type_msg == "new":
+            message = "NOVA INSCRIÇÃO: " + message
+
+            url = os.getenv("URL_TELEGRAM_SEND_MSG") + message
+
+            try:
+                response_msg = requests.post(url)
+                response_msg.raise_for_status()
+
+            except Exception as e:
+                    raise e
+
+            archive_type = os.path.splitext(enrollment.local_proof)[1]
+
+            if archive_type == ".pdf":
+                url_proof = os.getenv("URL_TELEGRAM_SEND_DOC")
+                key = "document"
+            else:
+                url_proof = os.getenv("URL_TELEGRAM_SEND_IMG")
+                key = "photo"
+
+            with open(enrollment.local_proof, "rb") as proof:
+                try:
+                    response_proof = requests.post(
+                        url_proof,
+                        data={
+                            "chat_id": os.getenv("ID_GRUPO_INSCRICOES"),
+                            "caption": f"Comprovante inscrição Nr {enrollment.id}"
+                        },
+                        files={
+                            key: proof
+                        }
+                    )
+
+                    response_proof.raise_for_status()
+
+                except Exception as e:
+                    raise e
+
+        elif type_msg == "update":
+            message = "INSCRIÇÃO ATUALIZADA: " + message
+
+            url = os.getenv("URL_TELEGRAM_SEND_MSG") + message
+
+            try:
+                response_msg = requests.post(url)
+                response_msg.raise_for_status()
+
+            except Exception as e:
+                raise e
