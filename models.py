061ed6d751d1d289c4db61f6e5d739d19a848ded
Filipe Cabral Coutinho <filipe_my_profile@MacBook-Pro-de-Weback.local>
2026-03-13 12:52:07 -0300

    feat: Adding a field and changing the type of the CPF field within the Enrollment model

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 from db import db
-from sqlalchemy import Integer, String, Column
+from datetime import datetime, timezone
 
 class Enrollment(db.Model):
     __tablename__ = "Enrollment"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False, unique=True)
-    cpf = db.Column(db.Integer, nullable=False, unique=True)
+    cpf = db.Column(db.String, nullable=False, unique=True)
     church = db.Column(db.String, nullable=False)
     celphone = db.Column(db.String, nullable=False)
     emergency_contact = db.Column(db.String, nullable=False)
@@ -15,3 +15,7 @@ class Enrollment(db.Model):
     hour_remedy = db.Column(db.String)
     local_proof = db.Column(db.String, nullable=False)
     payment_status = db.Column(db.String, nullable=False)
+    created_at = db.Column(
+        db.DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc)
+    )
